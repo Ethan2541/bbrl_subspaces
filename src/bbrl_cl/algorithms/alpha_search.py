8b27b3d8db61f1c55da1ea919fd19d41245e085d
Ethan2541 <eluong41@gmail.com>
2024-07-01 09:49:20 +0200

    Finding the best policy from an optimized subspace

--- src/bbrl_cl/algorithms/alpha_search.py
+++ src/bbrl_cl/algorithms/alpha_search.py
@@ -38,13 +38,14 @@ class AlphaSearch:
             n_rollouts = self.cfg.n_rollouts
             n_steps = self.cfg.n_validation_steps
 
-            # Estimating best alphas in the current subspace
+            # Estimating best alphas in the subspace
             alphas = Dirichlet(torch.ones(n_anchors)).sample(torch.Size([n_samples]))
             alphas = torch.stack([alphas for _ in range(self.cfg.time_size)], dim=0)
             values = []
 
-            logger.message("Starting value estimation in the new subspace")
+            logger.message("Starting value estimation in the subspace")
             _training_start_time = time.time()
+            # K-shot adaptation where K is the number of estimations
             for _ in range(self.cfg.n_estimations):
                 replay_workspace = replay_buffer.get_shuffled(alphas.shape[1])
                 replay_workspace.set_full("alphas",alphas)
@@ -53,31 +54,10 @@ class AlphaSearch:
                 values.append(replay_workspace["critic-1/q_values"].mean(0))
 
             values = torch.stack(values, dim=0).mean(0)
-            best_alphas = alphas[0, values.topk(n_rollouts//2).indices]
+            best_alphas = alphas[0, values.topk(n_rollouts).indices]
             info["best_alphas"] = best_alphas
-            logger.message("Estimated best alpha in the current subspace is : " + str(list(map(lambda x: round(x,2), best_alphas[0].tolist()))))
+            logger.message("Estimated best alpha in the subspace is : " + str(list(map(lambda x: round(x,2), best_alphas[0].tolist()))))
             logger.message("Time elapsed: " + str(round(time.time() - _training_start_time, 0)) + " sec")
-            
-            # Estimating best alphas in the former subspace
-            alphas = Dirichlet(torch.ones(n_anchors-1)).sample(torch.Size([n_samples]))
-            alphas = torch.cat([alphas, torch.zeros(*alphas.shape[:-1], 1)], dim=-1)
-            alphas = torch.stack([alphas for _ in range(self.cfg.time_size)], dim=0)
-            values = []
-
-            logger.message("Starting value estimation in the former subspace")
-            _training_start_time = time.time()
-            for _ in range(self.cfg.n_estimations):
-                replay_workspace = replay_buffer.get_shuffled(alphas.shape[1])
-                replay_workspace.set_full("alphas",alphas)
-                with torch.no_grad():
-                    critic_agent(replay_workspace)
-                values.append(replay_workspace["critic-1/q_values"].mean(0))
-
-            values = torch.stack(values, dim=0).mean(0)
-            best_alphas_before_training = alphas[0, values.topk(n_rollouts - n_rollouts//2).indices]
-            info["best_alphas_before_training"] = best_alphas_before_training
-            logger.message("Estimated best alpha in the former subspace is : " + str(list(map(lambda x: round(x,2), best_alphas_before_training[0].tolist()))))
-            logger.message("Time elapsed: " + str(round(time.time() - _training_start_time, 0)) + " sec")            
             
             del replay_workspace
             del alphas
@@ -88,49 +68,19 @@ class AlphaSearch:
             B = self.cfg.n_rollouts
             task._env_agent_cfg["n_envs"] = B
             _, env_agent = task.make()
-            alphas = torch.cat([best_alphas,best_alphas_before_training],dim = 0)
 
             action_agent.eval()
             acquisition_agent = TemporalAgent(Agents(env_agent, action_agent))
             acquisition_agent.seed(seed)
             w = Workspace()
             with torch.no_grad():
-                acquisition_agent(w, t = 0, n_steps= n_steps, alphas = alphas)
+                acquisition_agent(w, t=0, n_steps=n_steps, alphas=best_alphas)
 
             logger.message("Acquisition ended")
-            cumulative_rewards, cumulative_rewards_before_training =  w["env/cumulated_reward"][-1].chunk(2)
+            cumulative_rewards =  w["env/cumulated_reward"][-1]
             best_reward = cumulative_rewards.max()
-            best_reward_before_training = cumulative_rewards_before_training.max()
             best_alpha = best_alphas[cumulative_rewards.argmax()]
-            best_alpha_before_training = best_alphas_before_training[cumulative_rewards_before_training.argmax()]
 
-            #### debug
-            #print("-"*100)
-            #print("\n---best_reward:",best_reward)
-            #print("\n---best_reward_before_training:",best_reward_before_training)
-            #print("\n---best_alpha:",best_alpha)
-            #print("\n---best_alpha_before_training:",best_alpha_before_training)
-            #print("\n---best_alphas:\n",best_alphas)
-            #print("\n---best_alphas_before_training:\n",best_alphas_before_training)
-            #print("\n---cumulative_rewards:\n",cumulative_rewards)
-            #print("\n---cumulative_rewards_before_training:\n",cumulative_rewards_before_training)
-            #print("-"*100)
-
-            # Deciding to keep the anchor or not
-            logger.message("best_reward = " + str(round(best_reward.item(), 0))) 
-            logger.message("best_reward_before_training = " + str(round(best_reward_before_training.item(), 0))) 
-            logger.message("threshold = " + str(round(best_reward_before_training.item() * (1 + self.cfg.improvement_threshold), 0)))
-
-            if best_reward <= best_reward_before_training * (1 + self.cfg.improvement_threshold):
-                action_agent.remove_anchor(logger=logger)
-                best_alpha_before_training = best_alpha_before_training[:-1]
-                action_agent.set_best_alpha(alpha=best_alpha_before_training, logger=logger)
-                info["best_alpha"] = best_alpha_before_training
-            else:
-                action_agent.set_best_alpha(alpha=best_alpha, logger=logger)
-                info["best_alpha"] = best_alpha
-
-            r = {"n_epochs": 0, "training_time": time.time() -_training_start_time}
             del w
 
         # There is nothing to do if there is only a single policy: the only weight is set to 1 by default
