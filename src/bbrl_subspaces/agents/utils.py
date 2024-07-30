c56aecc9a9d00d5d93c9f5df71a2e894ed222aeb
Ethan2541 <eluong41@gmail.com>
2024-07-30 18:29:04 +0200

    Removed dependency to bbrl_gymnasium

--- src/bbrl_subspaces/agents/utils.py
+++ src/bbrl_subspaces/agents/utils.py
@@ -1,7 +1,6 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 #
-import bbrl_gymnasium
 import copy
 import os
 import torch
